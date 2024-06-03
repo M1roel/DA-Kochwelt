e802c82b477b8392321b99250a935f9ca7f0097a
M1roel <ppfautsch@yahoo.de>
2024-06-03 13:02:45 +0200

    CSS aufteilt in style.css und content.css

---
+++ src/js/rezept-peter.js
@@ -0,0 +1,45 @@
+const data_peter = {
+    recipename: "Brokkoli-Blumenkohl-Gratin",
+    author: "Peter",
+    date: "03.06.2024",
+    totalduration: "40 Min.",
+    preparationsduration: "30 Min.",
+    image: '<img src="../../public/img/gratin.jpg">',
+    ingredients: [
+      {
+        name: "Brokkoli",
+        unit: "g",
+        size: 250,
+      },
+      {
+        name: "Blumenkohl",
+        unit: "g",
+        size: 150,
+      },
+      {
+        name: "Petersilie",
+        unit: "Bund",
+        size: 0.25,
+      },
+      {
+        name: "Käse",
+        unit: "g",
+        size: 25,
+      },
+      {
+        name: "Haselnüsse",
+        unit: "g",
+        size: 25,
+      },
+      {
+        name: "abger. Zitronenschale (Bio)",
+        unit: "TL",
+        size: 0.5,
+      },
+      {
+        name: "Butter",
+        unit: "g",
+        size: 20,
+      }
+    ]
+}
