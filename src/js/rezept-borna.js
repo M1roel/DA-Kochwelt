130481b609c45a13a107e1bd4226a81bb9ba8468
M1roel <ppfautsch@yahoo.de>
2024-06-05 09:14:20 +0200

    Rezeptdateien von Borna von ftp Server geladen

---
+++ src/js/rezept-borna.js
@@ -0,0 +1,45 @@
+const data = {
+    recipename: 'Seehecht in Spinat-Sahne-Soße',
+    author: 'Borna',
+    date: '03.06.2024',
+    totalduration: '40 Min.',
+    preparationsduration: '20 Min.',
+    image: '<img src="./img/brownies">',
+    ingredients: [
+        {
+            name: "Seehecht",
+            unit: "g",
+            size: 150
+        },
+        {
+            name: "Kartoffeln",
+            unit: "g",
+            size: 200
+        },
+        {
+            name: "Babyspinat",
+            unit: "g",
+            size: 40
+        },
+        {
+            name: "Kochsahne",
+            unit: "g",
+            size: 100
+        },
+        {
+            name: "Geriebener Hartkäse",
+            unit: "g",
+            size: 20
+        },
+        {
+            name: "Semmelbrösel",
+            unit: "g",
+            size: 15
+        },
+        {
+            name: "Gemüsebrühe",
+            unit: "g",
+            size: 2
+        },
+    ]
+}
