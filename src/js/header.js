fdabfc16455b0ab65834cf0378cad76ad70d187f
M1roel <ppfautsch@yahoo.de>
2024-06-06 09:41:17 +0200

    Burger Menu Funktion für alles nochmals angepasst

--- src/js/header.js
+++ src/js/header.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelector(".hamburger-menu-button").addEventListener("click", function() {
     var navLinks = document.getElementById("myLinks");
     navLinks.classList.toggle("open");
+    console.log('klick');
   });
 });
